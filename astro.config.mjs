84f5f8e2d7682eb4ea71b8e0b3474bc1a329d1bb
wave <wave301@icloud.com>
2023-11-26 13:43:06 +0200

    fix(deploy): tried replacing deno.dev with vercel.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 //import cloudflare from "@astrojs/cloudflare";
-import deno from "@astrojs/deno";
+//import deno from "@astrojs/deno";
+//import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: deno()
+  adapter: vercel(),
 });
