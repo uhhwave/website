1c575e3ac3986562401c0999b887b847642c3597
wave <wave301@icloud.com>
2023-11-26 13:24:36 +0200

    fix(deploy): tried replacing pages.dev with deno.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
+//import cloudflare from "@astrojs/cloudflare";
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: cloudflare()
+  adapter: deno()
 });
