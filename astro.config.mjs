056cf0f9347b036616079bff38f3a38bb1e7c1fa
wave <wave301@icloud.com>
2023-11-26 13:48:41 +0200

    chore: try to readd cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-//import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare";
 //import deno from "@astrojs/deno";
 //import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless";
@@ -10,5 +10,12 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: process.env.CF_PAGES === '1' ? cloudflare() : vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    }
+  }),
 });
