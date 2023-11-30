1f14ec472b8785f1d67553ac33dffa8d9f9f3f90
wave <wave301@icloud.com>
2023-12-01 01:01:18 +0200

    chore(config): get rid of cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
-//import deno from "@astrojs/deno";
-//import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: process.env.CF_PAGES === '1' ? cloudflare() : vercel({
+  adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
