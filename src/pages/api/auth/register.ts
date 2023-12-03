f5997053c6a2a0cad9241915d7c5d23e7106b458
wave <wave301@icloud.com>
2023-12-03 18:02:51 +0200

    chore: add usernames

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -5,14 +5,20 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
   const email = formData.get("email")?.toString();
   const password = formData.get("password")?.toString();
+  const username = formData.get("username")?.toString();
 
-  if (!email || !password) {
-    return new Response("Email and password are required", { status: 400 });
+  if (!email || !password || !username) {
+    return new Response("Email, username and password are required", { status: 400 });
   }
 
   const { error } = await supabase.auth.signUp({
     email,
     password,
+    options: {
+      data: {
+        username: username
+      },
+    }
   });
 
   if (error) {
