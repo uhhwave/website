3c4c8eaf91cd655b989fc4eb65cd7d4e6fe1f7ab
wave <wave301@icloud.com>
2024-01-15 03:04:18 +0200

    chore: remove auth stuff and update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,12 +10,6 @@ export default defineConfig({
   adapter: vercel({
     webAnalytics: {
       enabled: true,
-    },
-    speedInsights: {
-      enabled: true,
     }
-  }),
-  experimental: {
-    devOverlay: true,
-  }
+  })
 });
