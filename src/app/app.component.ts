ba3c8f8f4db87da277fc13147f721c96cbee2444
wave <wave301@icloud.com>
2024-05-26 18:13:47 +0300

    refactor: optimize file locations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,12 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { NgOptimizedImage } from '@angular/common';
-import { trigger, style, animate, transition } from '@angular/animations';
-import {
-  Platform
-} from '@angular/cdk/platform';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, NgOptimizedImage],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.sass',
-  animations: [
-    trigger('fade', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate(700, style({ opacity: 1 })),
-      ]),
-    ]),
-  ],
+  imports: [RouterOutlet, CommonModule],
+  template: '<router-outlet></router-outlet>',
+  styles: [],
 })
-export class AppComponent implements OnInit {
-  title = '-website';
-  imageUrl = '';
-
-  constructor(public platform: Platform) {}
-
-  ngOnInit(): void {
-    let isHidden = true;
-
-    const buttons = document.querySelectorAll('#b');
-
-    buttons.forEach((button) => {
-      button.addEventListener('click', () => {
-        toggleElements();
-      });
-    });
-
-    const toggleElements = () => {
-      isHidden = !isHidden;
-      const s = document.getElementById('s')!;
-      const b = document.getElementById('b')!;
-      s.style.display = 'flex';
-      b.style.display = 'none';
-    };
-  }
-}
+export class AppComponent {}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,54 @@
+import { Component, HostBinding, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { NgOptimizedImage } from '@angular/common';
+import { trigger, style, animate, transition } from '@angular/animations';
+import { Platform } from '@angular/cdk/platform';
+import { isPlatformBrowser } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule, NgOptimizedImage],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(700, style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
+})
+export class HomeComponent implements OnInit {
+  title = '-website';
+  imageUrl = '';
+
+  constructor(
+    public platform: Platform,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit(): void {
+    let isHidden = true;
+
+    if (isPlatformBrowser(this.platformId)) {
+      const buttons = document.querySelectorAll('#b');
+
+      buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+          toggleElements();
+        });
+      });
+
+      const toggleElements = () => {
+        isHidden =!isHidden;
+        const s = document.getElementById('s')!;
+        const b = document.getElementById('b')!;
+        s.style.display = isHidden? 'flex' : 'none';
+        b.style.display = isHidden? 'none' : 'block';
+      };
+    }
+  }
+}
